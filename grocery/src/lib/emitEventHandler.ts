b92c46f617fd5b5c9063c6092e08198417fe13e0
rajprabhe <rajprabhe@gmail.com>
2026-09-11 13:24:00 +0530

    fix

--- grocery/src/lib/emitEventHandler.ts
+++ grocery/src/lib/emitEventHandler.ts
@@ -3,7 +3,7 @@ import axios from "axios"
 
 async function emitEventHandler(event:string, data:any, socketId?:string){
     try {
-        await axios.post(`${process.env.SOCKET_SERVER}/notify`,{socketId, event, data})
+        await axios.post(`${process.env.NEXT_PUBLIC_SOCKET_SERVER}/notify`,{socketId, event, data})
     } catch (error) {
         console.log(error)
     }
