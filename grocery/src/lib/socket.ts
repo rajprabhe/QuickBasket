fa09983a15adea66b06ad11ce01ddab1579327a9
rajprabhe <rajprabhe@gmail.com>
2026-09-11 12:12:53 +0530

    fixed value

--- grocery/src/lib/socket.ts
+++ grocery/src/lib/socket.ts
@@ -4,7 +4,7 @@ let socket:Socket | null = null
 
 export const getSocket = ()=>{
     if(!socket){
-        socket = io(process.env.NEXT_PUBLIC_SOCKET_SERVER)
+        socket = io(process.env.SOCKET_SERVER)
     }
     return socket
 }
